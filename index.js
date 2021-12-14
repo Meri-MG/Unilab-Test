cf9cadac10dfb39af99fcee1a9ceb2b7d1680439
Meri <mgogi13@freeuni.edu.ge>
2021-12-14 16:12:11 +0400

    Add changes

--- index.js
+++ index.js
@@ -3,52 +3,60 @@ const modal = document.querySelector('.work-modal-cont');
 const clickedDiv = document.querySelectorAll('.image');
 const mainBody = document.querySelector('body');
 
-
 const imagesList = [
-    {
-        id: 1,
-        img: "images/Image1.png",
-        day: 4,
-    },
-    {
-        id: 2,
-        img: "images/image2.png",
-        day: 3,
-    },
-    {
-        id: 3,
-        img: "images/image3.png",
-        day: 2,
-    }
-]
+  {
+    id: 1,
+    img: 'images/Image1.png',
+    cardName: 'Header/Section',
+    day: 4,
+  },
+  {
+    id: 2,
+    img: 'images/image2.png',
+    cardName: 'Testimonials Section',
+    day: 3,
+  },
+  {
+    id: 3,
+    img: 'images/image3.png',
+    cardName: 'Sign Up Form',
+    day: 2,
+  },
+];
+
+const clearElement = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
 
 const imagesUI = () => {
-    imagesList.forEach((list) => {
-        imageCont.innerHTML +=`
+  clearElement(imageCont);
+  imagesList.forEach((list) => {
+    imageCont.innerHTML += `
         <div class="image" id="${list.id}">
         <img src=${list.img} alt="" class="image-target">
         <div class="relative">
         <i class="far fa-comment-alt"></i>
-        <small>Header/Section Component</small>
+        <small>${list.cardName}</small>
         <h1>Code And Grow Together</h1>
         <h2>Day <span>0${list.day}</span></h2>
     </div>
     </div>`;
-    })
-    
-}
+  });
+};
 
 window.addEventListener('DOMContentLoaded', () => {
-    imagesUI()
+  imagesUI();
 });
 
-
-const popupUI = () => {
-    imagesList.forEach((list) => {
-    modal.innerHTML += `<div class="popupWindow" id="${list.id}">
+const popupUI = (index) => {
+  clearElement(modal);
+  imagesList.forEach((list) => {
+    modal.innerHTML += `<div class="popupWindow" id="${index}">
     <div class="left-side" >
         <img src=${list.img} alt="">
-            <div class="relative">
+            <div class="relative-popup">
                 <i class="far fa-comment-alt"></i>
                 <small>Header/Section Component</small>
                 <h1>Code And Grow Together</h1>
@@ -63,22 +71,23 @@ const popupUI = () => {
         </div>
     </div>
     </div>`;
-})
+  });
 };
 
-
-
-
 imageCont.addEventListener('click', (e) => {
-    if (e.target.classList.contains('image-target') || e.target.classList.contains('relative') ) {
-        popupUI();
-        modal.classList.add('open');
-        mainBody.classList.add('fixed');
-    }
-  });
+  if (
+    e.target.classList.contains('image-target') ||
+    e.target.classList.contains('relative')
+  ) {
+    popupUI(imagesList.id);
+    modal.classList.add('open');
+    imageCont.classList.add('hidden');
+  }
+});
 
 window.onclick = (e) => {
-    if(e.target.className === "work-modal-cont open") {
-        e.target.style.display = "none";
-    }
-}
+  if (e.target.className === 'work-modal-cont open') {
+    modal.classList.remove('open');
+    imageCont.classList.remove('hidden');
+  }
+};
